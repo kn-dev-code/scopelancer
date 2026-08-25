443998e14a3902b8441b73f6b061afd4db87378f
Kyle <nettervillekyle@gmail.com>
2026-08-25 18:37:09 -0500

    feat: Working on stripe products and prices & schemas

---
+++ app/web/scopelancer/lib/stripe/products/product.ts
@@ -0,0 +1,15 @@
+import { stripe } from "../stripe";
+
+
+/// Products
+export const starter = await stripe.products.create({
+
+})
+
+export const studio = await stripe.products.create({
+
+})
+
+export const pro = await stripe.products.create({
+    
+})
--- app/web/scopelancer/lib/stripe/stripe.ts
+++ app/web/scopelancer/lib/stripe/stripe.ts
@@ -1,4 +1,5 @@
 import Stripe from "stripe";
+import { prisma } from "../betterauth/auth";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2026-06-24.dahlia",
