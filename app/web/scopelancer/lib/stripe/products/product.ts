7e7f2b4525bc7fa9b261704b6369b7e77017c9b6
Kyle <nettervillekyle@gmail.com>
2026-08-25 18:39:32 -0500

    feat: Fixed Typescript & Prettier Errors

--- app/web/scopelancer/lib/stripe/products/product.ts
+++ app/web/scopelancer/lib/stripe/products/product.ts
@@ -1,15 +1,8 @@
 import { stripe } from "../stripe";
 
-
 /// Products
-export const starter = await stripe.products.create({
+//export const starter = await stripe.products.create({});
 
-})
+//export const studio = await stripe.products.create({});
 
-export const studio = await stripe.products.create({
-
-})
-
-export const pro = await stripe.products.create({
-    
-})
+//export const pro = await stripe.products.create({});
