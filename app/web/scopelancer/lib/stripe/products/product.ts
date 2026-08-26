ce61d68214537ba1aa9315d29c04d9117ce1f585
Kyle <nettervillekyle@gmail.com>
2026-08-26 17:41:40 -0500

    feat: Working on stripe api

--- app/web/scopelancer/lib/stripe/products/product.ts
+++ app/web/scopelancer/lib/stripe/products/product.ts
@@ -1,7 +1,41 @@
+import { prisma } from "@/lib/betterauth/auth";
 import { stripe } from "../stripe";
+import { HTTP_STATUS } from "@/lib/error_codes/error-code";
+import { NextResponse } from "next/server";
 
 /// Products
-//export const starter = await stripe.products.create({});
+
+export async function getProduct(database: string) {
+  const findProductId = await prisma.product.findUnique({
+    where: { id: database },
+  });
+
+  if (!findProductId) {
+    return NextResponse.json(
+      {
+        message: "Not found",
+      },
+      { status: HTTP_STATUS.NOT_FOUND },
+    );
+  }
+
+  const product = await stripe.products.create({
+    id: findProductId.id,
+    name: findProductId.name,
+    active: findProductId.active,
+  });
+
+  if (!product) {
+    return NextResponse.json(
+      {
+        message: "Not Found",
+      },
+      { status: HTTP_STATUS.NOT_FOUND },
+    );
+  }
+}
+
+export async function PATCH(database: string) {}
 
 //export const studio = await stripe.products.create({});
 
