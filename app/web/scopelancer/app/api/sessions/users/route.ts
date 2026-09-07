405710e1db1d2098a8ae3b44949733dbb9d7fc1c
Kyle <nettervillekyle@gmail.com>
2026-09-07 14:15:33 -0500

    feat: Fixing typecheck

--- app/web/scopelancer/app/api/sessions/users/route.ts
+++ app/web/scopelancer/app/api/sessions/users/route.ts
@@ -31,43 +31,4 @@ export async function GET(request: NextRequest) {
 }
 
 // POST /api/sessions/users
-export async function POST(request: NextRequest) {
-  try {
-    const session = await sessionAuth();
 
-    if (!session) {
-      return NextResponse.json(
-        { error: "Unauthorized" },
-        { status: HTTP_STATUS.UNAUTHORIZED },
-      );
-    }
-    const body = await request.json();
-    const { title, description, clientName, companyName, sessionStatus } = body;
-    if (!title) {
-      return NextResponse.json(
-        { error: "Not Found" },
-        { status: HTTP_STATUS.NOT_FOUND },
-      );
-    }
-    const newSession = await prisma.appSession.create({
-      data: {
-        title,
-        description,
-        clientName,
-        companyName,
-        userId: session.user.id,
-        sessionStatus: sessionStatus || "NOTSTARTED",
-      },
-    });
-
-    return NextResponse.json(
-      { success: true, session: newSession },
-      { status: HTTP_STATUS.CREATED },
-    );
-  } catch (e) {
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: HTTP_STATUS.INTERNAL_SERVER_ERROR },
-    );
-  }
-}
