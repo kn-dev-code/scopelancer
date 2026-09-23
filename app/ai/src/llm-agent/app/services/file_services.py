4744b34ead5b7d82a43d61ccae6c62a05bca8231
Kyle <nettervillekyle@gmail.com>
2026-09-23 17:01:00 -0500

    feat: Working on logic for database and file handling

---
+++ app/ai/src/llm-agent/app/services/file_services.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI, Request, Response
+# Handling files with userid
+"""
+def file_service_controller(request: Request, response: Response):
+"""
---
+++ app/ai/src/llm-agent/app/services/user_services.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, Request
+from sqlmodel import select
+
+# Handling user controls
+"""
+def post_user_controller(request: Request, user_id: str):
+"""
